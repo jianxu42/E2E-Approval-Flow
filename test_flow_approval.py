8c16dbb2114f161b960aeec5e8e5c41b58cfb188
jianxu42 <jianxu1@microsoft.com>
2022-12-30 12:02:37 +0800

    add more expects

--- test_flow_approval.py
+++ test_flow_approval.py
@@ -115,6 +115,8 @@ def test_approval_teams(page: Page):
     page.wait_for_timeout(10000)
     page.screenshot(path="teams.png", full_page=True)
 
+    locator = page.locator('text = Approved')
+    expect(locator).to_contain_text("Approved")
     expect(page).to_have_url(re.compile(".*teams.*"))
 
 
@@ -143,4 +145,6 @@ def test_approval_mail(page: Page):
     page.wait_for_timeout(10000)
     page.screenshot(path="mail.png", full_page=True)
 
+    locator = page.locator('text = Approved')
+    expect(locator).to_contain_text("Approved")
     expect(page).to_have_url(re.compile(".*mail.*"))
