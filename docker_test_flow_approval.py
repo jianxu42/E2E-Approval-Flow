8e2a9dd9b25ca7abbdfe9b09f29796057e0e060d
jianxu42 <jianxu1@microsoft.com>
2023-01-01 16:40:40 +0800

    test

--- docker_test_flow_approval.py
+++ docker_test_flow_approval.py
@@ -1,49 +1,49 @@
-# import datetime as dt
-# import os
-# from datetime import datetime as dt_dt
-# from typing import Generator
-#
-# import pytest
-# from playwright.sync_api import expect, Playwright, APIRequestContext, BrowserContext
-#
-# APPROVAL_FLOW_TITLE_FOR_PORTAL = ''
-# PORTAL_FLOW_LOCATION = ''
-# TEST_USER = os.environ['TEST_USER']
-# TEST_PWD = os.environ['TEST_PWD']
-# TEST_FLOW = os.environ['TEST_FLOW']
-# TEST_APPROVAL_PORTAL = os.environ['TEST_APPROVAL_PORTAL']
-#
-#
-# @pytest.fixture(scope="session")
-# def api_request_context(
-#         playwright: Playwright,
-# ) -> Generator[APIRequestContext, None, None]:
-#     headers = {
-#         "Accept": "application/json",
-#     }
-#     request_context = playwright.request.new_context(
-#         base_url="https://make.powerautomate.com",
-#         extra_http_headers=headers
-#     )
-#     yield request_context
-#     portal_flow_run = request_context.get(PORTAL_FLOW_LOCATION)
-#     assert portal_flow_run.json()["outcome"] == "Approve"
-#     request_context.dispose()
-#
-#
-# def test_trigger_approval_flow(api_request_context: APIRequestContext) -> None:
-#     global APPROVAL_FLOW_TITLE_FOR_PORTAL
-#     APPROVAL_FLOW_TITLE_FOR_PORTAL = f'test_approval_portal@{dt_dt.now(dt.timezone.utc).strftime("%Y-%m-%d %H:%M:%S:%f")}'
-#     approval_flow_for_portal_data = {
-#         "title": APPROVAL_FLOW_TITLE_FOR_PORTAL,
-#         "tag": "pytest",
-#     }
-#     approval_flow_for_portal_flow_run = api_request_context.post(TEST_FLOW, data=approval_flow_for_portal_data)
-#     global PORTAL_FLOW_LOCATION
-#     PORTAL_FLOW_LOCATION = approval_flow_for_portal_flow_run.headers["location"]
-#     assert approval_flow_for_portal_flow_run.ok
-#
-#
+import datetime as dt
+import os
+from datetime import datetime as dt_dt
+from typing import Generator
+
+import pytest
+from playwright.sync_api import Playwright, APIRequestContext
+
+APPROVAL_FLOW_TITLE_FOR_PORTAL = ''
+PORTAL_FLOW_LOCATION = ''
+TEST_USER = os.environ['TEST_USER']
+TEST_PWD = os.environ['TEST_PWD']
+TEST_FLOW = os.environ['TEST_FLOW']
+TEST_APPROVAL_PORTAL = os.environ['TEST_APPROVAL_PORTAL']
+
+
+@pytest.fixture(scope="session")
+def api_request_context(
+        playwright: Playwright,
+) -> Generator[APIRequestContext, None, None]:
+    headers = {
+        "Accept": "application/json",
+    }
+    request_context = playwright.request.new_context(
+        base_url="https://make.powerautomate.com",
+        extra_http_headers=headers
+    )
+    yield request_context
+    # portal_flow_run = request_context.get(PORTAL_FLOW_LOCATION)
+    # assert portal_flow_run.json()["outcome"] == "Approve"
+    request_context.dispose()
+
+
+def test_trigger_approval_flow(api_request_context: APIRequestContext) -> None:
+    global APPROVAL_FLOW_TITLE_FOR_PORTAL
+    APPROVAL_FLOW_TITLE_FOR_PORTAL = f'test_approval_portal@{dt_dt.now(dt.timezone.utc).strftime("%Y-%m-%d %H:%M:%S:%f")}'
+    approval_flow_for_portal_data = {
+        "title": APPROVAL_FLOW_TITLE_FOR_PORTAL,
+        "tag": "pytest",
+    }
+    approval_flow_for_portal_flow_run = api_request_context.post(TEST_FLOW, data=approval_flow_for_portal_data)
+    global PORTAL_FLOW_LOCATION
+    PORTAL_FLOW_LOCATION = approval_flow_for_portal_flow_run.headers["location"]
+    assert approval_flow_for_portal_flow_run.ok
+
+
 # def test_approval_portal(context: BrowserContext):
 #     context.tracing.start(screenshots=True, snapshots=True, sources=True)
 #     page = context.new_page()
