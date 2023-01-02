2955925470f943131704cbe8b377cff8f39923f4
jianxu42 <jianxu1@microsoft.com>
2023-01-02 15:54:01 +0800

    test timeout

--- test_flow_approval_mail.py
+++ test_flow_approval_mail.py
@@ -65,11 +65,10 @@ def test_approval_mail(context: BrowserContext):
         with page.expect_popup() as page_info:
             page.get_by_role("menuitem", name="Open in new window").click()
         popup_page = page_info.value
-        page.wait_for_timeout(10000)
+        popup_page.wait_for_timeout(10000)
         popup_page.get_by_role("button", name="Approve").click()
         popup_page.get_by_role("button", name="Submit").click()
 
-        popup_page.wait_for_timeout(10000)
         locator = popup_page.locator("'Approved'")
         expect(locator).to_contain_text("Approved")
 
