3fb399d3b7dff2fccbed40b93f8aaa58dec4d7b0
jianxu42 <jianxu1@microsoft.com>
2023-01-03 11:56:13 +0800

    update

--- docker_test_flow_approval.py
+++ docker_test_flow_approval.py
@@ -111,15 +111,16 @@ def test_approval_mail(context: BrowserContext):
     page.get_by_role("button", name="Yes").click()
 
     page.get_by_text(APPROVAL_FLOW_TITLE_FOR_MAIL).first.click()
-    page.wait_for_timeout(10000)
+    page.wait_for_timeout(3000)
     page.get_by_role("menuitem", name="More mail actions").click()
     page.get_by_role("menuitem", name="View").filter(has_text="View").click()
     with page.expect_popup() as page_info:
         page.get_by_role("menuitem", name="Open in new window").click()
     popup_page = page_info.value
-    popup_page.wait_for_timeout(10000)
+    popup_page.wait_for_timeout(3000)
     popup_page.get_by_role("button", name="Approve").click()
     popup_page.get_by_role("button", name="Submit").click()
+    popup_page.wait_for_timeout(3000)
 
     locator = popup_page.locator("'Approved'")
     expect(locator).to_contain_text("Approved")
