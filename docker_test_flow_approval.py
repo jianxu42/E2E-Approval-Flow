d258facfcf30a24b678a6e238b39f1214b3c5f97
jianxu42 <jianxu1@microsoft.com>
2023-01-07 09:30:56 +0800

    update

--- docker_test_flow_approval.py
+++ docker_test_flow_approval.py
@@ -94,6 +94,8 @@ def test_approval_portal(context: BrowserContext):
     page.get_by_role("button", name="Yes").click()
     logging.info("Login portal successful!")
 
+    if page.get_by_role("button", name="Close").is_visible():
+        page.get_by_role("button", name="Close").click()
     page.get_by_role("button", name=f"{APPROVAL_FLOW_TITLE_FOR_PORTAL}").click()
     page.get_by_text("Select an option").click()
     page.get_by_role("option", name="Approve").click()
