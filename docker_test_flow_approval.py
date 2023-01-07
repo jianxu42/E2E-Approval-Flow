17cf2dbae260399e27385769f8c46a25b3f3b995
jianxu42 <jianxu1@microsoft.com>
2023-01-07 21:01:57 +0800

    update

--- docker_test_flow_approval.py
+++ docker_test_flow_approval.py
@@ -129,7 +129,7 @@ def test_approval_mail(context: BrowserContext):
     with page.expect_popup() as page_info:
         page.get_by_role("menuitem", name="Open in new window").click()
     popup_page = page_info.value
-    popup_page.set_default_timeout(timeout=60000)
+    popup_page.set_default_timeout(timeout=120000)
     popup_page.wait_for_load_state(state="networkidle")
     popup_page.get_by_role("button", name="Approve").click()
     popup_page.get_by_role("button", name="Submit").click()
