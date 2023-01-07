8cab0a54b54a6d75875f4c224319c8354b201e86
jianxu42 <jianxu1@microsoft.com>
2023-01-08 00:09:29 +0800

    update

--- cleanup_flow_approval.py
+++ cleanup_flow_approval.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import re
 
 from playwright.sync_api import expect, BrowserContext
 
@@ -23,10 +25,11 @@ def test_cleanup_approval_flow(context: BrowserContext):
     page.wait_for_load_state()
     if page.get_by_role("button", name="Close").is_visible():
         page.get_by_role("button", name="Close").click()
-    for item in page.get_by_text(APPROVAL_FLOW_TITLE).all():
+    for item in page.get_by_text(re.compile(APPROVAL_FLOW_TITLE)).all():
         item.click()
         page.get_by_text("Select an option").click()
         page.get_by_role("option", name="Approve").click()
         page.get_by_role("button", name="Confirm").click()
         locator = page.locator("'Respond: Approve'")
         expect(locator).to_contain_text("Respond: Approve", timeout=30000)
+        logging.info(f"Found and cleanup {item}!")
