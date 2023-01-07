f2e4f42e74fda84eb90f754ac20f5333fccae69c
jianxu42 <jianxu1@microsoft.com>
2023-01-07 08:25:40 +0800

    update

--- test_flow_approval_mail.py
+++ test_flow_approval_mail.py
@@ -64,8 +64,8 @@ def test_approval_mail(context: BrowserContext):
         page.get_by_role("button", name="Yes").click()
         logging.info("Login mail successful!")
 
-        page.get_by_text(APPROVAL_FLOW_TITLE_FOR_MAIL).first.click()
         page.wait_for_timeout(random.randrange(6000, 12000))
+        page.get_by_text(APPROVAL_FLOW_TITLE_FOR_MAIL).first.click()
         page.get_by_role("menuitem", name="More mail actions").click()
         page.get_by_role("menuitem", name="View").filter(has_text="View").click()
         with page.expect_popup() as page_info:
