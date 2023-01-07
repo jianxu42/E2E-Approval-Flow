2e4ca8b872690d236ba2e1ca49d280bddfdbe7cf
jianxu42 <jianxu1@microsoft.com>
2023-01-07 16:57:10 +0800

    update

--- test_flow_approval_portal.py
+++ test_flow_approval_portal.py
@@ -63,12 +63,13 @@ def test_approval_portal(context: BrowserContext):
         page.get_by_role("button", name="Yes").click()
         logging.info("Login portal successful!")
 
-        page.wait_for_timeout(random.randrange(1000, 3000))
-        if page.get_by_role("button", name="Close").is_visible():
-            page.get_by_role("button", name="Close").click()
         if not page.get_by_role("button", name=f"{APPROVAL_FLOW_TITLE_FOR_PORTAL}").is_visible():
             page.reload()
             logging.warning("Reloaded the approval portal page!")
+            if page.get_by_role("button", name="Close").is_visible():
+                page.get_by_role("button", name="Close").click()
+                logging.warning("Found pop-up window and closed it!")
+
         page.get_by_role("button", name=f"{APPROVAL_FLOW_TITLE_FOR_PORTAL}").click()
         page.get_by_text("Select an option").click()
         page.get_by_role("option", name="Approve").click()
