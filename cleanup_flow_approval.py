8859d5b27f32debd40b661f7536f0ab9d767aa63
jianxu42 <jianxu1@microsoft.com>
2023-01-08 11:13:31 +0800

    update

--- cleanup_flow_approval.py
+++ cleanup_flow_approval.py
@@ -1,7 +1,8 @@
 import logging
 import os
 
-from playwright.sync_api import BrowserContext
+import pytest
+from playwright.async_api import BrowserContext
 
 APPROVAL_FLOW_TITLE = 'test_approval'
 TEST_USER = os.environ['TEST_USER']
@@ -9,29 +10,30 @@ TEST_PWD = os.environ['TEST_PWD']
 TEST_APPROVAL_PORTAL = os.environ['TEST_APPROVAL_PORTAL']
 
 
-def test_cleanup_approval_flow(context: BrowserContext):
-    page = context.new_page()
-    page.goto(TEST_APPROVAL_PORTAL)
-    page.get_by_placeholder("Email, phone, or Skype").click()
-    page.get_by_placeholder("Email, phone, or Skype").fill(TEST_USER)
-    page.get_by_role("button", name="Next").click()
-    page.get_by_placeholder("Password").click()
-    page.get_by_placeholder("Password").fill(TEST_PWD)
-    page.get_by_role("button", name="Sign in").click()
-    page.get_by_role("button", name="Yes").click()
+@pytest.mark.asyncio
+async def test_cleanup_approval_flow(context: BrowserContext):
+    page = await context.new_page()
+    await page.goto(TEST_APPROVAL_PORTAL)
+    await page.get_by_placeholder("Email, phone, or Skype").click()
+    await page.get_by_placeholder("Email, phone, or Skype").fill(TEST_USER)
+    await page.get_by_role("button", name="Next").click()
+    await page.get_by_placeholder("Password").click()
+    await page.get_by_placeholder("Password").fill(TEST_PWD)
+    await page.get_by_role("button", name="Sign in").click()
+    await page.get_by_role("button", name="Yes").click()
 
-    page.wait_for_load_state("networkidle")
-    if page.get_by_role("button", name="Close").is_visible():
-        page.get_by_role("button", name="Close").click()
+    await page.wait_for_load_state("networkidle")
+    if await page.get_by_role("button", name="Close").is_visible():
+        await page.get_by_role("button", name="Close").click()
     while True:
-        if len(page.get_by_role("button", name=APPROVAL_FLOW_TITLE).all()) > 0:
+        if len(await page.get_by_role("button", name=APPROVAL_FLOW_TITLE).all()) > 0:
             li = page.get_by_role("button", name=APPROVAL_FLOW_TITLE).first
             title = li.text_content()
-            li.click()
-            page.get_by_text("Select an option").click()
-            page.get_by_role("option", name="Approve").click()
-            page.get_by_role("button", name="Confirm").click()
-            page.get_by_role("button", name="Done").click()
+            await li.click()
+            await page.get_by_text("Select an option").click()
+            await page.get_by_role("option", name="Approve").click()
+            await page.get_by_role("button", name="Confirm").click()
+            await page.get_by_role("button", name="Done").click()
             logging.info(f"Cleanup {title}!")
         else:
             break
