87abb3fffbcb058111fd06b4dfa3e839786be878
jianxu42 <jianxu1@microsoft.com>
2023-01-08 13:43:15 +0800

    update

--- docker_test_flow_approval.py
+++ docker_test_flow_approval.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import logging
 import os
+import time
 from datetime import datetime as dt_dt
 from typing import Generator
 
@@ -112,7 +113,6 @@ def test_approval_portal(context: BrowserContext):
 
 def test_approval_mail(context: BrowserContext):
     page = context.new_page()
-    page.set_default_timeout(timeout=60000)
     page.goto(TEST_APPROVAL_MAIL)
 
     page.get_by_placeholder("Email, phone, or Skype").click()
@@ -126,18 +126,22 @@ def test_approval_mail(context: BrowserContext):
 
     page.wait_for_load_state()
     page.get_by_text(APPROVAL_FLOW_TITLE_FOR_MAIL).first.click()
-    page.get_by_role("menuitem", name="More mail actions").click()
-    page.get_by_role("menuitem", name="View").filter(has_text="View").click()
-    with page.expect_popup() as page_info:
-        page.get_by_role("menuitem", name="Open in new window").click()
-    popup_page = page_info.value
-    popup_page.set_default_timeout(timeout=120000)
-    popup_page.wait_for_load_state()
-    popup_page.wait_for_load_state("networkidle")
-    popup_page.get_by_role("button", name="Approve").click()
-    popup_page.get_by_role("button", name="Submit").click()
+    # this depends on the render time for adaptive card from the server, set 8 seconds for now
+    time.sleep(8)
+    with page.expect_popup() as popup:
+        page.get_by_text(APPROVAL_FLOW_TITLE_FOR_MAIL).first.dblclick()
+    page_popup = popup.value
+    if page_popup.get_by_role("button", name="Approve").is_visible():
+        page_popup.get_by_role("button", name="Approve").click()
+    else:
+        page_popup.close()
+        with page.expect_popup() as popup:
+            page.get_by_text(APPROVAL_FLOW_TITLE_FOR_MAIL).first.dblclick()
+        page_popup = popup.value
+        page_popup.get_by_role("button", name="Approve").click()
+    page_popup.get_by_role("button", name="Submit").click()
 
-    locator = popup_page.locator("'Approved'")
+    locator = page_popup.locator("'Approved'")
     expect(locator).to_contain_text("Approved", timeout=30000)
     logging.info("Approved from mail!")
 
