b3196b1478e1711a757be8dfc6392872eac2a6db
jianxu42 <jianxu1@microsoft.com>
2023-01-08 09:17:47 +0800

    update

--- cleanup_flow_approval.py
+++ cleanup_flow_approval.py
@@ -24,7 +24,7 @@ def test_cleanup_approval_flow(context: BrowserContext):
     page.wait_for_load_state()
     if page.get_by_role("button", name="Close").is_visible():
         page.get_by_role("button", name="Close").click()
-    for li in page.locator(f"text={APPROVAL_FLOW_TITLE}").all():
+    for li in page.locator("'test_approval'").all():
         li.click()
         page.get_by_text("Select an option").click()
         page.get_by_role("option", name="Approve").click()
